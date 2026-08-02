3c5882a9071a1598718b0e9c7968f704b5c26b05
Enrique <ep847632@gmail.com>
2026-08-01 23:11:35 -0600

    dashboard premium Bloom

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,24 +1,88 @@
 console.log("Bloom components cargado");
 
 
+
 async function loadComponent(id, file){
 
+
     const element = document.getElementById(id);
+
 
     if(!element) return;
 
 
+
     const response = await fetch(file);
+
 
     const html = await response.text();
 
 
+
     element.innerHTML = html;
+
+
+
+    setActiveSidebar();
+
+
 
 }
 
 
-// Cargar componentes
+
+
+
+function setActiveSidebar(){
+
+
+
+    const currentPage = window.location.pathname
+    .split("/")
+    .pop();
+
+
+
+    const links = document.querySelectorAll(
+        ".sidebar-menu a"
+    );
+
+
+
+    links.forEach(link => {
+
+
+
+        const linkPage = link
+        .getAttribute("href");
+
+
+
+        if(linkPage === currentPage){
+
+
+            link.classList.add("active");
+
+
+        }else{
+
+
+            link.classList.remove("active");
+
+
+        }
+
+
+
+    });
+
+
+
+}
+
+
+
+
 
 loadComponent(
     "sidebar-container",
